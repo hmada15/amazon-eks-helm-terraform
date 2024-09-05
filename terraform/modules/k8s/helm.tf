9f24d0a83e99c52024d539e927a5bcf5a484623a
hmada15 <31375621+hmada15@users.noreply.github.com>
2024-09-05 14:31:28 +0300

    add cert manager

--- terraform/modules/k8s/helm.tf
+++ terraform/modules/k8s/helm.tf
@@ -76,3 +76,20 @@ resource "helm_release" "external_nginx" {
 
   depends_on = [helm_release.aws_lbc]
 }
+
+resource "helm_release" "cert_manager" {
+  name = "cert-manager"
+
+  repository       = "https://charts.jetstack.io"
+  chart            = "cert-manager"
+  namespace        = "cert-manager"
+  create_namespace = true
+  version          = "v1.15.0"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+
+  depends_on = [helm_release.external_nginx]
+}
